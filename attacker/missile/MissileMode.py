aa0da4d86f5865c798cddaafb0166ac205b963ee
UNIQUEXIONG <uniquexiong123@gmail.com>
2023-07-05 17:03:09 +0800

    delete unused class

--- attacker/missile/MissileMode.py
+++ attacker/missile/MissileMode.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 
 # 导弹的三种飞行模式，由于三种导弹飞行模式都相同，因此抽出来
-class SubsonicMode:
-    STRAIGHT_LINE = 1  # 直线飞行模式
-    PARABOLA = 2  # 抛物线模式
-    DIVE = 3  # 俯冲
+# class Mode:
+#     STRAIGHT_LINE = 1  # 直线飞行模式
+#     PARABOLA = 2  # 抛物线模式
+#     DIVE = 3  # 俯冲
 
 
 class MissileModeAbstract(ABC):
