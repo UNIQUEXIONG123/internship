fb879a5ff3d83e25748b355e4ac7026130da646b
UNIQUEXIONG <uniquexiong123@gmail.com>
2023-07-07 20:22:10 +0800

    finish plane mode

--- attacker/plane/PlaneMode.py
+++ attacker/plane/PlaneMode.py
@@ -1,3 +1,4 @@
+import math
 import random
 from abc import ABC, abstractmethod
 from enum import Enum
@@ -83,7 +84,7 @@ class BomberMode(BomberModeAbstract):
     def __init__(self):
         super().__init__()
         self.start_distance = random.gauss(29000, 3670)  # 18-40公里
-        self.start_height = random.gauss(550, 90)
+        self.start_height = random.gauss(4000, 1000)
         self.start_direction = random.uniform(0, 359)
         self.start_speed = random.gauss(335, 38)
         self.speed = random.gauss(self.start_speed, 1)  # 有一米的误差，这个字段表示在飞行过程中速度保持不变
@@ -121,32 +122,43 @@ class BomberMode(BomberModeAbstract):
 # 攻击机俯冲飞行模式
 class FighterMode(FighterModeAbstract):
     def set_threaten_level(self, level):
-        pass
+        self.threaten_level = level
 
     def get_distance(self, t):
-        pass
+        # 斜边长
+        hypotenuse = math.sqrt(self.start_distance ** 2 + self.start_height ** 2)
+        horizontal_velocity = self.start_distance / hypotenuse * self.speed
+        return abs(self.start_distance - horizontal_velocity * t)
 
     def get_height(self, t):
-        pass
+        hypotenuse = math.sqrt(self.start_distance ** 2 + self.start_height ** 2)
+        vertical_velocity = self.start_height / hypotenuse * self.speed
+        return abs(self.start_height - vertical_velocity * t)
 
     def get_direction(self, t):
-        pass
+        hypotenuse = math.sqrt(self.start_distance ** 2 + self.start_height ** 2)
+        vertical_velocity = self.start_height / hypotenuse * self.speed
+        if self.start_height - vertical_velocity * t > 0:
+            return self.start_direction
+        else:
+            return (self.start_direction + 180) % 360
 
     def get_speed(self, t):
-        pass
+        return self.speed
 
     def get_heading(self, t):
-        pass
+        return self.start_heading
 
     def get_countermeasure(self, t):
-        pass
+        return self.countermeasure
 
     def __init__(self):
         super().__init__()
         self.start_distance = random.gauss(29000, 3670)
-        self.start_height = random.gauss(1050, 317)
+        self.start_height = random.gauss(550, 150)
         self.start_direction = random.uniform(0, 359)
         self.start_speed = random.gauss(600, 67)
+        self.speed = random.gauss(self.start_speed, 1)  # 有一米的误差，这个字段表示在飞行过程中速度保持不变
         self.start_heading = random.gauss(0, 0.33)
         self.start_time = 0
         self.threaten_level = get_plane_threat_level(self.start_distance, AircraftType.FIGHTER)
@@ -162,30 +174,35 @@ class HelicopterMode(HelicopterAbstract):
     def set_threaten_level(self, level):
         self.threaten_level = level
 
+    def get_distance(self, t):
+        return abs(self.start_distance - self.speed * t)
+
     def get_height(self, t):
-        pass
+        return self.start_height
 
     def get_direction(self, t):
-        pass
+        if self.start_distance - self.speed * t > 0:
+            return self.start_direction
+        else:
+            return (self.start_direction + 180) % 360
 
     def get_speed(self, t):
-        pass
+        return self.speed
 
     def get_heading(self, t):
-        pass
+        return self.start_heading
 
     def get_countermeasure(self, t):
-        pass
+        return self.countermeasure
 
-    def get_distance(self, t):
-        pass
-
+    # 直升机的高度设置de很低，掠海高度
     def __init__(self):
         super().__init__()
-        self.start_distance = random.gauss(29000, 3670)
-        self.start_height = random.gauss(225, 58)
+        self.start_distance = random.gauss(20000, 3333)  # 路径和轰炸机一样，但是更低
+        self.start_height = random.gauss(11.5, 2.8)
         self.start_direction = random.uniform(0, 359)
         self.start_speed = random.gauss(95, 8)
+        self.speed = random.gauss(self.start_speed, 1)  # 有一米的误差，这个字段表示在飞行过程中速度保持不变
         self.start_heading = random.gauss(0, 0.33)
         self.start_time = 0
         self.threaten_level = get_plane_threat_level(self.start_distance, AircraftType.HELICOPTER)
@@ -199,22 +216,25 @@ class HelicopterMode(HelicopterAbstract):
 # 运输机的高空直线飞行模式
 class TransportMode(TransportAbstract):
     def set_threaten_level(self, level):
-        pass
+        self.threaten_level = level
 
     def get_distance(self, t):
-        pass
+        return abs(self.start_distance - self.speed * t)
 
     def get_height(self, t):
-        pass
+        return self.start_height
 
     def get_direction(self, t):
-        pass
+        if self.start_distance - self.speed * t > 0:
+            return self.start_direction
+        else:
+            return (self.start_direction + 180) % 360
 
     def get_speed(self, t):
-        pass
+        return self.speed
 
     def get_heading(self, t):
-        pass
+        return self.start_heading
 
     def get_countermeasure(self, t):
         return 0
@@ -222,9 +242,10 @@ class TransportMode(TransportAbstract):
     def __init__(self):
         super().__init__()
         self.start_distance = random.gauss(29000, 3670)
-        self.start_height = random.gauss(12500, 833)
+        self.start_height = random.gauss(12500, 1833)
         self.start_direction = random.uniform(0, 359)
         self.start_speed = random.gauss(225, 8)
+        self.speed = random.gauss(self.start_speed, 1)  # 有一米的误差，这个字段表示在飞行过程中速度保持不变
         self.start_heading = random.gauss(0, 0.33)
         self.start_time = 0
         self.threaten_level = get_plane_threat_level(self.start_distance, AircraftType.TRANSPORT)
